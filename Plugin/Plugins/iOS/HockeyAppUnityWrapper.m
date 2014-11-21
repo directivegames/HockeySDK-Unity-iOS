04ea90a70e362672e57386d350a1933220251969
Andreas Axelsson <axl@directivegames.com>
2014-11-21 18:16:11 +0800

    Fixed compilation errors with Unity 5 and Xcode 6.1.

--- Plugin/Plugins/iOS/HockeyAppUnityWrapper.m
+++ Plugin/Plugins/iOS/HockeyAppUnityWrapper.m
@@ -3,33 +3,35 @@
 
 void HockeyApp_StartHockeyManager(char *appID, char *serverURL, char *authType, char *secret, bool updateManagerEnabled, bool autoSendEnabled) {
   
-  [HockeyAppUnity startManagerWithIdentifier:[NSString stringWithUTF8String:appID]
-                                   serverURL:[NSString stringWithUTF8String:serverURL]
+    [HockeyAppUnity startManagerWithIdentifier:[NSString stringWithUTF8String:appID]
+                                   serverUrl:[NSString stringWithUTF8String:serverURL]
                                     authType:[NSString stringWithUTF8String:authType]
                                       secret:[NSString stringWithUTF8String:secret]
                         updateManagerEnabled:updateManagerEnabled
                              autoSendEnabled:autoSendEnabled];
 }
 
-void HockeyApp_ShowFeedbackListView(){
-  
-  [HockeyAppUnity showFeedbackListView];
+void HockeyApp_ShowFeedbackListView()
+{
+    [HockeyAppUnity showFeedbackListView];
 }
 
-char* HockeyApp_GetAppVersion(){
+char* HockeyApp_GetAppVersion()
+{
+    NSString* appVersion = [HockeyAppUnity appVersion];
+    NSUInteger length = [appVersion lengthOfBytesUsingEncoding:NSUTF8StringEncoding] + 1;
+    char* res = (char*)malloc(length);
+    [appVersion getCString:res maxLength:length encoding:NSUTF8StringEncoding];
   
-  NSString *appVersion = [[HockeyAppUnity appVersion] UTF8String];
-  char* res = (char*)malloc(strlen(appVersion) + 1);
-  strcpy(res, appVersion);
-  
-  return res;
+    return res;
 }
 
-char* HockeyApp_GetBundleIdentifier(){
-  
-  NSString *bundleIdentifier = [[HockeyAppUnity bundleIdentifier] UTF8String];
-  char* res = (char*)malloc(strlen(bundleIdentifier) + 1);
-  strcpy(res, bundleIdentifier);
-  
-  return res;
+char* HockeyApp_GetBundleIdentifier()
+{
+    NSString* bundleIdentifier = [HockeyAppUnity bundleIdentifier];
+    NSUInteger length = [bundleIdentifier lengthOfBytesUsingEncoding:NSUTF8StringEncoding] + 1;
+    char* res = (char*)malloc(length);
+    [bundleIdentifier getCString:res maxLength:length encoding:NSUTF8StringEncoding];
+
+    return res;
 }
